e285669eea9c82117e6df6f230377577c8ad8ac5
pedrolopez <p.lopez.ch.hr@hotmail.com>
2023-09-29 18:23:42 +0200

    implements Reverse functional interface method by a lambda expression; test passed Signed-off-by: pedrolopez <p.lopez.ch.hr@hotmail.com>

---
+++ src/n1Exe8/App.java
@@ -0,0 +1,26 @@
+package n1Exe8;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Reversable reverse = (frase) -> {
+			for (int i = frase.length() - 1; i != -1; i--) {
+				System.out.print(frase.charAt(i));
+			}
+			return frase;
+		};
+
+		reverse.reverse("etneup");
+	}
+
+	/*
+	 * Crea una Functional Interface que contenga un método llamado reverse(). Este
+	 * método debe recibir y debe devolver un String. En el main() de la clase
+	 * principal, inyecta a la interfaz creada mediante una lambda, el cuerpo del
+	 * método, de modo que devuelva la misma cadena que recibe como parámetro pero
+	 * al revés. Invoca la instancia de la interfaz pasándole una cadena y comprueba
+	 * si el resultado es correcto.
+	 */
+
+}
