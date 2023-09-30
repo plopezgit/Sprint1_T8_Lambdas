94311a77db99c078bfc463821729b90db2908f64
pedrolopez <p.lopez.ch.hr@hotmail.com>
2023-09-30 13:50:18 +0200

    fixes the name class

--- src/n3Exe1/Student.java
+++ src/n3Exe1/Student.java
@@ -1,13 +1,13 @@
 package n3Exe1;
 
-public class Studient {
+public class Student {
 	
 	private String studientName;
 	private int studientAge;
 	private String studientCourse;
 	private int studientNote;
 	
-	public Studient(String studientName, int studientAge, String studientCourse, int studientNote) {
+	public Student(String studientName, int studientAge, String studientCourse, int studientNote) {
 		this.studientName = studientName;
 		this.studientAge = studientAge;
 		this.studientCourse = studientCourse;
@@ -52,16 +52,4 @@ public class Studient {
 				+ studientCourse + ", studientNote=" + studientNote + "]";
 	}
 	
-
 }
-
-	/*
-	 * Crea una clase Alumno con sus atributos: Nombre, edad, curso y nota.
-	 * Llena una lista con 10 alumnos
-	 * Muestra por pantalla el nombre y la edad de cada alumno/a.
-	 * Filtra la lista por todos los alumnos cuyo nombre comienza por ‘a’. 
-	 * Asigna a estos alumnos a otra lista y muestra por pantalla la nueva lista (todo con lambdes).
-	 * Filtra y muestra por pantalla a los alumnos que tienen una nota de 5 o superior.
-	 * Filtra y muestra por pantalla a los alumnos que tienen una nota de 5 o más, y que no son de PHP.
-	 * Muestra a todos los alumnos que hacen JAVA y son mayores de edad.
-	 */ 
